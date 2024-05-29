60e1debee2fdd7a5c67a06a93bf75d428662e4cc
Talles Coelho <talescoelho43@gmail.com>
2024-05-29 12:29:11 -0300

    Adicionada dependência de carregamento de reação, App.jsx atualizado para usar o componente ReactLoading e lógica de busca e paginação de fotos modificada.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
+import ReactLoading from "react-loading";
 
 import { ThemeProvider } from "styled-components";
-import { Flex, Screen } from "./style";
+import { Button, Flex, Screen, Typography } from "./style";
 import { darkTheme, lightTheme } from "./style/theme";
 import { NavBar } from "./components/navbar";
 import { Header } from "./components/header";
@@ -10,29 +12,34 @@ import { Publications } from "./components/publications";
 import { getPhotos } from "./services/photo";
 
 function App() {
-  const PHOTOS_PER_PAGE = 20
+  const PHOTOS_PER_PAGE = 8;
 
   const [theme, setTheme] = useState("dark");
   const [photos, setPhotos] = useState([]);
   const [isLoading, setLoading] = useState(false);
-  const [photosPerPage, setPhotosPerPage] = useState(PHOTOS_PER_PAGE)
+  const [photosPerPage, setPhotosPerPage] = useState(PHOTOS_PER_PAGE);
 
-  const releaseLoading =  () => setLoading(false)
+  const releaseLoading = () => setLoading(false);
 
   const themeToggler = () => {
     theme === "light" ? setTheme("dark") : setTheme("light");
   };
 
+  const handlePhotosPerPage = () => {
+    setPhotosPerPage(photosPerPage + PHOTOS_PER_PAGE);
+  };
+
   async function fetchPhotos() {
-    const data = await getPhotos(photosPerPage, releaseLoading );
+    setLoading(true);
+    const data = await getPhotos(photosPerPage, releaseLoading);
     setPhotos(data);
   }
 
-  console.log(photos)
+  console.log(photos);
 
   useEffect(() => {
     fetchPhotos();
-  }, []);
+  }, [photosPerPage]);
 
   return (
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
@@ -41,8 +48,21 @@ function App() {
 
         <Flex>
           <Header />
-          <Stories photos={photos}/>
+          <Stories photos={photos} />
           <Publications photos={photos} />
+
+          {isLoading ? (
+            <ReactLoading
+              type="spinningBubbles"
+              color={theme.textPrimary}
+              height={20}
+              width={20}
+            />
+          ) : (
+            <Button onClick={handlePhotosPerPage}>
+              <Typography>Ver mais</Typography>
+            </Button>
+          )}
         </Flex>
       </Screen>
     </ThemeProvider>
